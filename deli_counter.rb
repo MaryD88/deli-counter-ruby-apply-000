4b6b06b38776567f96ece9b932392a61827090fd
proactive-researcher-8324 <marymichelle1118@gmail.com>
2020-10-01 19:08:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,12 +5,11 @@ def line(array)
     puts "The line is currently empty."
   else 
     message = "The line is currently:"
-    
+ 
   array.each_with_index do |name, index| 
   message += " #{index +1}. #{name}"
   end 
-  
-  puts "#{message}"
+   puts "#{message}"
   end 
 end   
 
@@ -19,6 +18,11 @@ def take_a_number(array, name)
   position = array.index(name) +1
   puts "Welcome, #{name}. You are number #{position} in line."
 end 
+
+count = 0
+def take_a_number
+  count += 1
+end
 
 def now_serving(array)
   if array.empty? == true 
